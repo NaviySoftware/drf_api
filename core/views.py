ac8b4be28a3ed5b50c423ee35057550397bd0e60
NaviySoftware <serj_navii@mail.ru>
2020-02-06 18:27:43 +0600

    Authorization lesson from justdjango lesson 3

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 # third party imports
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,6 +9,9 @@ from .serializers import PostSerializer
 from .models import Post
 
 class TestView(APIView):
+
+    permission_classes = (IsAuthenticated, )
+
     def get(self, request, *args, **kwargs):
         qs = Post.objects.all()
         serializer = PostSerializer(qs, many=True)
--- drf_api/urls.py
+++ drf_api/urls.py
@@ -1,10 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
 
+from rest_framework.authtoken.views import obtain_auth_token
+
 from core.views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('', TestView.as_view(), name='test')
+    path('', TestView.as_view(), name='test'),
+    path('api/token', obtain_auth_token, name='obtain-token'),
 ]
